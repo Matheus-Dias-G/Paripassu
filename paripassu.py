9ed0a1802063f42ce203eeb2f959e4b2f4844c99
Matheus-Dias-G <neotromd@gmail.com>
2023-11-27 21:18:24 -0300

    alterando valores| adicionando nome as tabelas

--- paripassu.py
+++ paripassu.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 from datetime import date
 from unidecode import unidecode
@@ -8,22 +9,32 @@ today = today.strftime('%d/%m/%y')
 providers = {'Antônio Marcos': '91.391.397/0001-45', 'Água de Coco':'91.393.397/0001-46'}
 
 
-top = 'documento elo erigem;data;nome produto;quantidade'
+top = ['Documento elo origem', 'Data', 'Nome produto', 'Embalagem', 'Capacidade embalagem', 'Unidade Embalagem', 'Quantidade']
 def main():
     if len(sys.argv) < 2:
         print('Uso: python paripassu.py <text>')
         sys.exit(1)
 
     text = sys.argv[1]
+    #make the separation between products
     text = text.split("\n")
     index_list = range(0, len(text))
     for n in index_list:
         text[n] = text[n].split(', ')
-        #print(text[n][0])
-
+        #adding data on text
+        text[n][1] = today
+        
         for provider in providers.keys():
             if unidecode(text[n][0].lower()) == unidecode(provider.lower()):
-                #text[n][0] =  providers[provider]
-                print(providers[provider])
+                text[n][0] =  providers[provider]
+ 
+    with open('/home/mathasx/Documentos/programming/paripassu_csv/tabela.csv', 'w') as f:
+        write = csv.writer(f)
+        print(top)
+        write.writerow(top)
+        for product in text:
+            write.writerow(product)
+        f.close()
+        print('Arquivo tabela.csv criado no diretorio:/home/mathasx/Documentos/programming/paripassu_csv/')
 if __name__ == "__main__":
     main()
